450ef08cb29056afe5c50feb36c8de2508eb5313
Akilan K <ponakilan25@gmail.com>
2024-02-11 09:51:51 +0530

    Dataset concatenation method added

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,52 +4,54 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class PowerConsumptionDataset(Dataset):
-    def __init__(self, sm_path, plugs_path, num_rows, sequence_length):
+    def __init__(self, sm_path: str, plugs_path: str, num_rows: int, sequence_length: int):
         self.sm_path = sm_path
         self.plugs_path = plugs_path
         self.num_rows = num_rows
         self.sequence_length = sequence_length
 
+        for i, file in enumerate(os.listdir(self.sm_path)):
+            if file.endswith(".csv"):
+                # Load the smart meter data and the plug data as pandas dataframes
+                sm_df = pd.DataFrame(pd.read_csv(os.path.join(self.sm_path, file)).iloc[:, 0])
+                plugs_01 = self.get_plug_df(file, '01')
+                plugs_02 = self.get_plug_df(file, '02')
+                plugs_03 = self.get_plug_df(file, '03')
+                plugs_04 = self.get_plug_df(file, '04')
+                plugs_05 = self.get_plug_df(file, '05')
+                plugs_06 = self.get_plug_df(file, '06')
+                plugs_07 = self.get_plug_df(file, '07')
+
+                if i == 0:
+                    # Concatenate all the dataframes in axis 1
+                    self.df = pd.concat([sm_df, plugs_01, plugs_02, plugs_03, plugs_04, plugs_05, plugs_06, plugs_07], axis=1)
+                else:
+                    # Concatenate all the dataframes in axis 0
+                    self.df = pd.concat([self.df, sm_df, plugs_01, plugs_02, plugs_03, plugs_04, plugs_05, plugs_06, plugs_07], axis=0)
+
     def __len__(self):
         return (self.num_rows * len(os.listdir(self.sm_path))) - self.sequence_length
     
-    def get_plug_sequence(self, filename, idx):
-        pass
-    
-    def get_sm_sequence(self, idx: int):
+    def get_plug_df(self, filename: str, folder: str) -> pd.DataFrame:
         """
-        Retrieve the sequence of the specified length from the smart meter data.
+        Open the specified plug file and return it as a pandas dataframe. If the file is not found, 
+        then return a dummy dataframe.
 
         Args:
-            idx (int): Index of the sequence
+            filename (str): Name of the file
+            folder (str): Name of the folder
+
         Returns:
-            sequence (tuple): Tuple containing the sequence and the label
+            df (pandas.DataFrame): Pandas dataframe containing the plug data
         """
 
-        # Calculate the file index, start index, and end index
-        file_idx = idx // self.num_rows
-        start_idx = idx % self.num_rows
-        end_idx = start_idx + self.sequence_length
-
-        # Get the file name and load the file as pandas as dataframe
-        sm_files = os.listdir(self.sm_path)
-        sm_files.sort()
-        sm_file = sm_files[file_idx]
-        sm_df = pd.read_csv(os.path.join(self.sm_path, sm_file), header=None)
-
-        # If the end index is greater than the number of rows in the file, then we need to load the next file
-        if end_idx + 1 > self.num_rows:
-            file_idx_2 = file_idx + 1
-            sm_file_2 = sm_files[file_idx_2]
-            sm_df_2 = pd.read_csv(os.path.join(self.sm_path, sm_file_2), header=None)
-            sm_df = pd.concat([sm_df, sm_df_2], axis=0)
-
-        # Get the sequence
-        sequence_X = sm_df.iloc[start_idx:end_idx, 0].values
-        sequence_y = sm_df.iloc[end_idx, 0]
-
-        return sequence_X, sequence_y
-
+        try:
+            file_path = os.path.join(self.plugs_path, os.path.join(folder, filename))
+            return pd.read_csv(file_path, header=None)
+        
+        except FileNotFoundError:
+            return pd.read_csv(os.path.join(self.plugs_path, 'plugs_dummy.csv'), header=None)
+    
     def __getitem__(self, idx):
         sm_sequence = self.get_sm_sequence(idx)
         return sm_sequence
