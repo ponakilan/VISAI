132f60c675d2e96f901bb2c67e023cd5215c6444
Akilan K <ponakilan25@gmail.com>
2024-02-12 18:54:15 +0530

    Model updated

--- models/model_1.py
+++ models/model_1.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 
-class MyModel(nn.Module):
+class ConsumptionPredictor(nn.Module):
     def __init__(self):
-        super(MyModel, self).__init__()
+        super(ConsumptionPredictor, self).__init__()
         
-        self.conv1d_1 = nn.Conv1d(in_channels=8, out_channels=32, kernel_size=3)
+        self.conv1d_1 = nn.Conv1d(in_channels=12, out_channels=32, kernel_size=3)
         self.conv1d_2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3)
         self.lstm = nn.LSTM(input_size=64, hidden_size=5, batch_first=True)
         self.linear = nn.Linear(in_features=5, out_features=1)
