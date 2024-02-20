a4452246527e295368788b1ae31fd03c02ec1392
ponakilan <ponakilan25@gmail.com>
2024-02-20 14:32:27 +0530

    Model save issue fixed

--- models/utils/train.py
+++ models/utils/train.py
@@ -1,7 +1,7 @@
 import torch
 from sklearn.metrics import r2_score
 
-def train_one_epoch(model, optimizer, criterion, dataloader, run, log_interval, device):
+def train_one_epoch(model, optimizer, criterion, dataloader, run, log_interval, device, save_path, epoch):
     model.train()
     running_loss = 0.0
     last_loss = 0.0
@@ -20,12 +20,13 @@ def train_one_epoch(model, optimizer, criterion, dataloader, run, log_interval, 
             run.log({"loss": last_loss})
             running_loss = 0.0
 
+    torch.save(model, f'{save_path}/model_{epoch}.pt')
     return last_loss, model
 
 def train(model, optimizer, criterion, train_dataloader, epochs, run, log_interval, device, save_path):
     for i in range(epochs):
         print(f"Epoch {i+1} started.")
-        epoch_loss, model = train_one_epoch(model, optimizer, criterion, train_dataloader, run, log_interval, device)
+        epoch_loss, model = train_one_epoch(model, optimizer, criterion, train_dataloader, run, log_interval, device, save_path, i)
         outputs_l, targets_l = [], []
         for i, (sequences, targets) in enumerate(train_dataloader):
             outputs = model(sequences.to(device))
@@ -34,5 +35,4 @@ def train(model, optimizer, criterion, train_dataloader, epochs, run, log_interv
         r2 = r2_score(targets_l, outputs_l)
         run.log({"R2_Score": r2})
         print(f"Epoch loss: {epoch_loss}\nAccuracy: {r2}")
-        torch.save(model, f'{save_path}/model_{i}.pt')
         return model
