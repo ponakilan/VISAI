dce3cc513b997d97b785442114bcfb27fdd15124
ponakilan <ponakilan25@gmail.com>
2024-02-20 08:16:28 +0530

    Training metrics improved

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -17,25 +17,25 @@ class PowerConsumptionDataset(Dataset):
         # Generate the dataset
         self.generate_dataset()
 
-    def get_plug_df(self, filename: str, folder: str) -> pd.DataFrame:
-        """
-        Open the specified plug file and return it as a pandas dataframe. If the file is not found, 
-        then return a dummy dataframe.
+    # def get_plug_df(self, filename: str, folder: str) -> pd.DataFrame:
+    #     """
+    #     Open the specified plug file and return it as a pandas dataframe. If the file is not found, 
+    #     then return a dummy dataframe.
 
-        Args:
-            filename (str): Name of the file
-            folder (str): Name of the folder
+    #     Args:
+    #         filename (str): Name of the file
+    #         folder (str): Name of the folder
 
-        Returns:
-            df (pandas.DataFrame): Pandas dataframe containing the plug data
-        """
+    #     Returns:
+    #         df (pandas.DataFrame): Pandas dataframe containing the plug data
+    #     """
 
-        try:
-            file_path = os.path.join(self.plugs_path, os.path.join(folder, filename))
-            return pd.read_csv(file_path, header=None)
+    #     try:
+    #         file_path = os.path.join(self.plugs_path, os.path.join(folder, filename))
+    #         return pd.read_csv(file_path, header=None)
         
-        except FileNotFoundError:
-            return pd.read_csv(os.path.join(self.plugs_path, 'plugs_dummy.csv'), header=None)
+    #     except FileNotFoundError:
+    #         return pd.read_csv(os.path.join(self.plugs_path, 'plugs_dummy.csv'), header=None)
 
     def generate_dataset(self):
         """
@@ -44,51 +44,22 @@ class PowerConsumptionDataset(Dataset):
         """
         sm_files = os.listdir(self.sm_path)
         sm_files.sort()
+        self.data = pd.DataFrame()
         for file in sm_files:
-            if not os.path.exists(os.path.join(self.data_dir, f'concat/{file}')) and file.endswith(".csv"):
-                # Load the smart meter data and the plug data as pandas dataframes
-                sm_df = pd.DataFrame(pd.read_csv(os.path.join(self.sm_path, file)).iloc[:, 0])
-                plugs_01 = self.get_plug_df(file, '01')
-                plugs_02 = self.get_plug_df(file, '02')
-                plugs_03 = self.get_plug_df(file, '03')
-                plugs_04 = self.get_plug_df(file, '04')
-                plugs_05 = self.get_plug_df(file, '05')
-                plugs_06 = self.get_plug_df(file, '06')
-                plugs_07 = self.get_plug_df(file, '07')
-
-                # Concatenate all the dataframes in axis 1 and save it as the final dataframe
-                df = pd.concat([sm_df, plugs_01, plugs_02, plugs_03, plugs_04, plugs_05, plugs_06, plugs_07], axis=1)
-                df.to_csv(os.path.join(self.data_dir, f'concat/{file}'), index=False)
+            sm_df = pd.read_csv(f'{self.sm_path}/{file}').iloc[:, 0]
+            self.data = pd.concat([self.data, sm_df], ignore_index=True)
+            if self.data.shape[0] > self.max_len:
+                break
 
     def __len__(self):
-        if self.max_len > len(os.listdir(os.path.join(self.data_dir, 'concat')))*86400 - self.sequence_length - 1:
-            return len(os.listdir(os.path.join(self.data_dir, 'concat')))*86400 - self.sequence_length - 1
-        else:
-            return self.max_len
+        return self.data.shape[0] - self.sequence_length - 1
     
-    @functools.lru_cache(maxsize=None)
     def __getitem__(self, idx):
-        # Get the list of files
-        files = os.listdir(os.path.join(self.data_dir, 'concat'))
-
-        # Calculate the file index
-        file_idx = idx // self.num_rows
-
-        # Calculate the start and end index
-        start_idx = idx % self.num_rows
+        start_idx = idx
         end_idx = start_idx + self.sequence_length
 
-        # Open the file
-        df = pd.read_csv(os.path.join(self.data_dir, f'concat/{files[file_idx]}'))
-
-        # Check if there is need to open another file
-        if start_idx > (end_idx % self.num_rows):
-            file_idx_2 = file_idx + 1
-            df_2 = pd.read_csv(os.path.join(self.data_dir, f'concat/{files[file_idx_2]}'))
-            df = pd.concat([df, df_2], axis=0)
-
         # Get the sequence and target
-        sequence = df.iloc[start_idx:end_idx, :].values.astype(np.float32)
-        target = np.array(df.iloc[end_idx, 0], dtype=np.float32)
+        sequence = self.data.iloc[start_idx:end_idx, :].values.astype(np.float32)
+        target = np.array(self.data.iloc[end_idx, 0], dtype=np.float32)
 
         return sequence, target
