fea40acd984880ce86c9715610e54ab7a36e0523
ponakilan <ponakilan25@gmail.com>
2024-02-23 10:11:19 +0530

    Recommender optimized

--- ecoenergetix.py
+++ ecoenergetix.py
@@ -106,28 +106,23 @@ while sum(rec_1)/len(rec_1) > cur_avg or sum(rec_2)/len(rec_2) > cur_avg:
             diff = rec_sum_1 - rec_sum_2
         else:
             diff = rec_sum_2 - rec_sum_1
-        rec_1.append(random.choice(range(5, int(rec_sum_1))) + 12)
-        rec_2.append(random.choice(range(5, int(rec_sum_2))) + 6)
+        rec_1.append(random.choice(range(5, int(rec_sum_1))) + 22)
+        rec_2.append(random.choice(range(5, int(rec_sum_2))) + 16)
 
 while sum(rec_1)/len(rec_1) < 0.8 * cur_avg:
     print(True)
+    rec_temp = rec_1
     rec_1 = []
-    for i in forecasts:
-        if rec_sum_1 > rec_sum_2:
-            diff = rec_sum_1 - rec_sum_2
-        else:
-            diff = rec_sum_2 - rec_sum_1
-        rec_1.append(random.choice(range(5, int(rec_sum_1))) + 22)
+    for i in rec_temp:
+        rec_1.append(i*1.2)
+        
 
 while sum(rec_2)/len(rec_2) < 0.8 * cur_avg:
     print(True)
+    rec_temp = rec_2
     rec_2 = []
-    for i in forecasts:
-        if rec_sum_1 > rec_sum_2:
-            diff = rec_sum_1 - rec_sum_2
-        else:
-            diff = rec_sum_2 - rec_sum_1
-        rec_2.append(random.choice(range(5, int(rec_sum_2))) + 12)
+    for i in rec_temp:
+        rec_2.append(i*1.2)
 
 st.line_chart(
     pd.DataFrame({
